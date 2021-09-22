79a49f07133375dc7ec7df33d64632e5626f223c
Johnnymc2001 <37579854+Johnnymc2001@users.noreply.github.com>
2021-09-22 18:20:06 +0700

    Fix MySQL Working Slow AF

--- src/java/Utils/DBHelpers.java
+++ src/java/Utils/DBHelpers.java
@@ -12,7 +12,8 @@ import java.sql.SQLException;
 public class DBHelpers {
 
     public static Connection makeConnection() throws SQLException {
-//        // 1. Get current Context
+        try {
+            //        // 1. Get current Context
 //        Context context = new InitialContext();
 //        // 2. Get container Context
 //        Context tomcatContext = (Context) context.lookup("java:comp/env");
@@ -20,13 +21,16 @@ public class DBHelpers {
 //        DataSource ds = (DataSource) tomcatContext.lookup("Database");
 //        // 4. Get connection
 //        Connection con = ds.getConnection();
-        // 1. Load Driver
-        //        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        // 2. Create connection string
-        //        String url = "jdbc:sqlserver://localhost:1433;databaseName=Registeration";
-        // 3. Open Connection
-        //        Connection con = DriverManager.getConnection(url, "sa", "123456789");
-
+// 1. Load Driver
+//        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+// 2. Create connection string
+//        String url = "jdbc:sqlserver://localhost:1433;databaseName=Registeration";
+// 3. Open Connection
+//        Connection con = DriverManager.getConnection(url, "sa", "123456789");
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
         String connectionUrl
                 = "jdbc:sqlserver://swp391-group4.database.windows.net:1433;database=swp391_group4;user=usernameg4swp@swp391-group4;password=4!m3wp$MA835HU@6r&Fx;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
         Connection connection = DriverManager.getConnection(connectionUrl);
