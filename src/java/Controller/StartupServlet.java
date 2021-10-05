4bd5f623b2cb8a182ea229eaa8842d1289b73445
GemBlue071001 <trinhtam2001@gmail.com>
2021-10-05 18:42:06 +0700

    Create StartupServlet.java

---
+++ src/java/Controller/StartupServlet.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import DAO.AccountDAO;
+import DAO.AccountDTO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+@WebServlet(name = "StartupServlet", urlPatterns = {"/StartupServlet"})
+public class StartupServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+
+            ServletContext sc = request.getServletContext();
+        HashMap<String, String> roadmap = (HashMap<String, String>) sc.getAttribute("ROADMAP");
+        String url = "";
+        try {
+
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                
+                for (Cookie c : cookies) {
+                  
+                    String username = c.getName();
+                    if (!username.equals("JSESSIONID")) {
+
+                        String password = c.getValue();
+                      
+                        AccountDAO dao = new AccountDAO();
+                        AccountDTO dto = dao.checkLogin(username, password);
+
+                        if (null != dto) {
+
+                            HttpSession session = request.getSession();
+                            session.setAttribute("USERNAME", username);
+                            url="home";
+                            break;
+                        }
+                    } else {
+
+                    }
+                }
+
+            }
+
+        } finally {
+            url=roadmap.get(url);
+            response.sendRedirect(url);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(StartupServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(StartupServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
